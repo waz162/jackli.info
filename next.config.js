019b99248dc426643b62dd354a14927a2f27467e
Jack Li <asuka2345@gmail.com>
2023-08-19 00:57:53 -0400

    fixing next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    unoptimized: true
+  }
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
 }
 
 module.exports = nextConfig
