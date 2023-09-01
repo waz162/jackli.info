6407d7d687df9db66d67f437c14a7de1b3c94848
Jack Li <jack.li@whatifmediagroup.com>
2023-09-01 12:26:46 -0400

    changing animation background

--- src/app/Components/AnimatedBackgroundBlur.tsx
+++ src/app/Components/AnimatedBackgroundBlur.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 function AnimatedElements() {
-  const variants = {
+  const animationVariants = {
     start: {
       x: -100,
       y: -100,
@@ -13,12 +13,12 @@ function AnimatedElements() {
     end: {
       x: window.innerWidth + 100,
       y: window.innerHeight + 100,
-      rotate: 360, // Rotate the elements as they move
+      rotate: 5, // Rotate the elements as they move
       transition: {
-        duration: 2, // Adjust the duration for a faster movement
+        duration: 20, // Adjust the duration for a faster movement
         ease: "linear", // Linear easing for a consistent motion
         repeat: Infinity, // Loop the animation indefinitely
-        repeatType: "loop", // Restart the animation smoothly
+        repeatType: "reverse", // Restart the animation smoothly
       },
     },
   };
@@ -26,16 +26,16 @@ function AnimatedElements() {
   return (
     <div>
       <motion.div
-        className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#963e40] sm:w-[68.75rem]"
-        initial="initial"
-        animate="animate"
-        variants={variants}
+        className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fcc9ca] blur-[10rem] dark:bg-[#963e40] sm:w-[68.75rem]"
+        initial="start"
+        animate="end"
+        variants={animationVariants}
       ></motion.div>
       <motion.div
-        className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#49459a] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
-        initial="initial"
-        animate="animate"
-        variants={variants}
+        className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#bcb3ff] blur-[10rem] dark:bg-[#49459a] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        initial="start"
+        animate="end"
+        variants={animationVariants}
       ></motion.div>
     </div>
   );
